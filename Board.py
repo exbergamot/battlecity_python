2a604fcd64a175d50b2619e064f4f9e0bb8e3eec
Dmytro_Kushchov <dmytro_kushchov@epam.com>
2019-06-14 18:03:47 +0300

    wip

--- Board.py
+++ Board.py
@@ -4,7 +4,7 @@ Created on Wed Jun 12 11:03:49 2019
 
 @author: Dmytro_Kushchov
 """
-
+import numpy as np
 from enum import Enum
 from Tank import Tank, Eagle, Bullet
 
@@ -13,25 +13,70 @@ class Side(Enum) :
     BOTTOM = 2;
     
 class Action(Enum) :
-    UP = 1;
-    DOWN = 2;
-    LEFT = 3;
-    RIGHT = 4;
-    UP_AND_SHOOT = 5;
-    DOWN_AND_SHOOT = 6;
-    LEFT_AND_SHOOT = 7;
-    RIGHT_AND_SHOOT = 8;
+    UP =    (0, 1, False);
+    DOWN =  (0, -1, False);
+    LEFT =  (-1, 0, False);
+    RIGHT = (1, 0, False);
+    UP_AND_SHOOT =    (0, 1, True);
+    DOWN_AND_SHOOT =  (0, -1, True);
+    LEFT_AND_SHOOT =  (-1, 0, True);
+    RIGHT_AND_SHOOT = (1, 0, True);
 
 class Board :
     walls = [
-            [0,0,0,0,0,0,0]
-            [0,0,1,1,1,0,0]
-            [0,0,0,0,0,0,0]
-            [1,1,0,1,0,1,1]
-            [0,0,0,0,0,0,0]
-            [0,0,1,1,1,0,0]
-            [0,0,0,0,0,0,0]
+            [0,0,0,0,0,0,0],
+            [0,0,1,1,1,0,0],
+            [0,0,0,0,0,0,0],
+            [1,1,0,1,0,1,1],
+            [0,0,0,0,0,0,0],
+            [0,0,1,1,1,0,0],
+            [0,0,0,0,0,0,0],
     ];
     eagles = [Eagle(3,0,Side.TOP), Eagle(3,6,Side.BOTTOM)];
     tanks = [Tank(2,0,Side.TOP), Tank(4,0,Side.TOP), Tank(2,6,Side.BOTTOM), Tank(4,6,Side.BOTTOM)]
     bullets = []
+    
+    def toLayeredMap(self, tank) :
+        result = np.zeros((10,7,7));
+        #walls
+        result[0] = self.walls;
+        #eagles
+            #ally
+            for eagle in eagles :
+                result[tank.side.value 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
--- Tank.py
+++ Tank.py
@@ -5,18 +5,23 @@ Created on Wed Jun 12 10:26:52 2019
 @author: Dmytro_Kushchov
 """
 class Tank:
-    next_action;
-    bullet;
     def __init__(self, x, y, side) :
         self.x = x;
         self.y = y;
         self.side = side;
+        
+    def __str__(self) :
+        print('Tank x - {} y - {}'.format(self.x, self.y));
+    
 
 class Eagle:
     def __init__(self, x, y, side) :
         self.x = x;
         self.y = y;
         self.side = side;
+    
+    def __str__(self) :
+        print('Eagle x - {} y - {}'.format(self.x, self.y));
 
 class Bullet:
     def __init__(self, x, y, ax = 0, ay = 0) :
@@ -24,3 +29,7 @@ class Bullet:
         self.y = y;
         self.ax = ax;
         self.ay = ay;
+        
+def testFunction(message) :
+    print("func");
+    print(message);
